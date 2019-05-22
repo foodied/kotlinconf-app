10e20af99148fa50ed69aee67af77f5ca705d13b
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-05-22 16:02:12 +0300

    fixup! fixup! [WIP] update ios app

--- iosApp/iosApp/Controllers/SessionController.swift
+++ iosApp/iosApp/Controllers/SessionController.swift
@@ -1,12 +1,21 @@
 import Foundation
 import UIKit
+import youtube_ios_player_helper
 
 class SessionController : UIViewController {
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var videoBox: YTPlayerView!
+
+    @IBOutlet weak var speakerLabel: UILabel!
+    @IBOutlet weak var roomLabel: UILabel!
+    @IBOutlet weak var slidesLabel: UILabel!
 
     override func viewDidLoad() {
         startButton.setImage(UIImage(named: "favouriteEmpty.png"), for: .selected)
         startButton.setImage(UIImage(named: "favourite.png"), for: .selected)
+        videoBox.load(withVideoId: "wZZ7oFKsKzY")
+
+        let tapHandler = UIGestureRecognizer(target: <#T##Any?#>, action: <#T##Selector?#>)
     }
 
     @IBAction func backButtonTouch(_ sender: Any) {
